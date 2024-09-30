893a728d287bf6c37a559cb284eff6eee5b3ff5f
abhishekkhade9137 <abhishekkhade69@gmail.com>
2024-09-30 19:35:47 +0530

    changes to cadets page

--- app/dashboard/Cadets/page.js
+++ app/dashboard/Cadets/page.js
@@ -1,12 +1,10 @@
-
 import React from 'react'
 import Newcadet from './_components/newcadet'
+
 function Cadets() {
   return (
-    <div className=''>
-      <h2 className=' text-2xl font-extrabold justify-between flex items-center'>Cadets<Newcadet/></h2>
-      
-      
+    <div className='h-screen flex justify-center items-center'>
+      <Newcadet />
     </div>
   )
 }
